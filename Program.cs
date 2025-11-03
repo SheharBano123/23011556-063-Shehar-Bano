bf623021e40077fea8dd2c7f3930e0d0fb867e81
SheharBano123 <23011556-063@uog.edu.pk>
2025-11-03 22:19:24 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Assignment_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.method();
+            
+        }
+    }
+}
